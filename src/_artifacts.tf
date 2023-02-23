9e9d78777d723ef1db87a4fb3f058183ad75825d
Chris Hill <chris@massdriver.cloud>
2023-02-23 09:40:24 -0800

    Add artifact

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,17 +1,40 @@
-# resource "massdriver_artifact" "<name>" {
-#   field                = "the field in the artifacts schema"
-#   provider_resource_id = "AWS ARN or K8S SelfLink"
-#   name                 = "a contextual name for the artifact"
-#   artifact = jsonencode(
-#     {
-#       # data = {
-#       #   # This should match the aws-rds-arn.json schema file
-#       #   arn = "aws::..."
-#       # }
-#       # specs = {
-#       #   # Any existing spec in ./specs
-#       #   # aws = {}
-#       # }
-#     }
-#   )
-# }
+locals {
+    capabilities_ingress_public = var.cluster.ingress.enable_ingress ? [{
+        load_balancer_arn = aws_lb.main.0.arn
+        security_group_arn = aws_security_group.alb.0.arn
+        listeners = [{
+            arn = aws_lb_listener.https.0.arn
+            port = aws_lb_listener.https.0.port
+            protocol = lower(aws_lb_listener.https.0.protocol)
+            domains = flatten([
+                local.r53_zone_ids_to_domains[local.default_route53_zone_id],
+                [ for zone in local.additional_route53_zone_ids : local.r53_zone_ids_to_domains[zone] ]
+            ])
+        }]
+    }] : []
+}
+
+resource "massdriver_artifact" "cluster" {
+  field                = "cluster"
+  provider_resource_id = aws_ecs_cluster.main.arn
+  name                 = "ECS Cluster artifact for ${var.md_metadata.name_prefix}"
+  artifact = jsonencode(
+    {
+      data = {
+        capabilities = {
+            ingress = local.capabilities_ingress_public
+        }
+        infrastructure = {
+            arn = aws_ecs_cluster.main.arn
+            vpc = var.vpc
+        }
+        security = {}
+      }
+      specs = {
+        aws = {
+            region = var.vpc.specs.aws.region
+        }
+      }
+    }
+  )
+}
