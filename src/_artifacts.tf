093c9e0fe623acc97a5e12339223c796cac33a40
Chris Hill <chris@massdriver.cloud>
2023-02-23 09:41:17 -0800

    terraform fmt

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,17 +1,17 @@
 locals {
-    capabilities_ingress_public = var.cluster.ingress.enable_ingress ? [{
-        load_balancer_arn = aws_lb.main.0.arn
-        security_group_arn = aws_security_group.alb.0.arn
-        listeners = [{
-            arn = aws_lb_listener.https.0.arn
-            port = aws_lb_listener.https.0.port
-            protocol = lower(aws_lb_listener.https.0.protocol)
-            domains = flatten([
-                local.r53_zone_ids_to_domains[local.default_route53_zone_id],
-                [ for zone in local.additional_route53_zone_ids : local.r53_zone_ids_to_domains[zone] ]
-            ])
-        }]
-    }] : []
+  capabilities_ingress_public = var.cluster.ingress.enable_ingress ? [{
+    load_balancer_arn  = aws_lb.main.0.arn
+    security_group_arn = aws_security_group.alb.0.arn
+    listeners = [{
+      arn      = aws_lb_listener.https.0.arn
+      port     = aws_lb_listener.https.0.port
+      protocol = lower(aws_lb_listener.https.0.protocol)
+      domains = flatten([
+        local.r53_zone_ids_to_domains[local.default_route53_zone_id],
+        [for zone in local.additional_route53_zone_ids : local.r53_zone_ids_to_domains[zone]]
+      ])
+    }]
+  }] : []
 }
 
 resource "massdriver_artifact" "cluster" {
@@ -22,17 +22,17 @@ resource "massdriver_artifact" "cluster" {
     {
       data = {
         capabilities = {
-            ingress = local.capabilities_ingress_public
+          ingress = local.capabilities_ingress_public
         }
         infrastructure = {
-            arn = aws_ecs_cluster.main.arn
-            vpc = var.vpc
+          arn = aws_ecs_cluster.main.arn
+          vpc = var.vpc
         }
         security = {}
       }
       specs = {
         aws = {
-            region = var.vpc.specs.aws.region
+          region = var.vpc.specs.aws.region
         }
       }
     }
